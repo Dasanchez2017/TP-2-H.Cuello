443ca0f1746ad0a4d4f05e9a01b7e08f569b7293
Dasanchez2017 <33604328+Dasanchez2017@users.noreply.github.com>
2017-11-12 15:29:09 -0300

    Update main.cpp
    Pusimos 3 módulos
    solo nos da el error al compilar del tInfo (que no supimos como solucionarlo)
    faltan los módulos de ProcPoli - ArmarPoli y SumarPoli (este ultimo lo tenemos medio hecho pero me parece mejor empezarlo de cero)
    tenemos que definir los 2 grupos y hacer  la parte teórica de la presentación

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <iomanip.h>
+#include <stdio.h>
+#include <conio.h>
+#define LOW_VALUE -1
 
 using namespace std;
+
+
+
+typedef int tInfo;
+
+typedef struct tnodo *tLista;
+
+struct tnodo {
+    tInfo Info;
+    tInfo Coe;
+    tInfo Exp;
+    tLista Sgte;
+};
+
+void SacarPrimerNodo(tLista &Lista, tInfo &valor) {
+    tLista pElim;
+
+    pElim = Lista;
+    valor = Lista->Info;
+    Lista = Lista->Sgte;
+    delete pElim;
+
+}
 
 int main()
 {
@@ -35,17 +62,32 @@ SumarPoli (tLista &LstPoliP, tLista &LstPoliQ, tLista &LstPoliSum);
 
 }
 
-EmitirPoli (tLista &LstPoliX, str4 nomPoliX);
-{
+void EmitirPoli(tLista &LstPoliX, str4 nomPoliX) {
+	tLista PoliAux;
 
-}
+	cout << endl << nomPoliX << " = ";
+	PoliAux = Px;
+	while (PoliAux != NULL) {
+		cout << PoliAux->Info.coef << " ";
+		if (PoliAux->Info.expo > 0)
+			cout << "x^" << PoliAux->Info.expo << " +";
+		PoliAux = PoliAux->Sgte;
+	}
+	cout << '\b' << " presione enter para continuar...";
+	getch();
+} // Emitir Polinomio
 
-LecEspPoli (tArc PoliX, sReg &rPoliX, bool &fdaPoliX);
-{
 
-}
+void LecEspPoli(tLista PoliX,sReg &rPoliX,bool &fdaPoliX ) {
+	if (PoliX != NULL)
+		SacarPrimerNodo(PoliX,rPoliX);
+	else
+		rPoliX.Exp = LOW_VALUE;
+};
 
-LecEspNodo (tLista &LstPoliX, sNodo &rInfoPoliX, bool &fdlPoliX);
-{
-
+void LecEspNodo(tLista &LstPoliX,sNodo &rInfoPoliX,bool &fdlPoliX ) {
+	if (LstPoliX != NULL)
+		SacarPrimerNodo(LstPoliX,rInfoPoliX);
+	else
+		rInfoPoliX.Info = LOW_VALUE;
 }
